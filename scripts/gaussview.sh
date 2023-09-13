1a6e7d78bb6157c3de2100ab8350ecff9c4be0d5
James <144615809+JamesOBrien2@users.noreply.github.com>
2023-09-13 16:33:59 +0100

    Made it dynamic to the directory & enabled use on a file

--- scripts/gaussview.sh
+++ scripts/gaussview.sh
@@ -1,6 +1,20 @@
-# Loads g16 & executes GaussView
-gaussview() {
-    qrsh -l short
-    module load apps/binapps/gaussian/g16c01_em64t
-    gv &
+function gaussview {
+        # Store the current working directory
+        cwd=$(pwd)
+
+        # Run qrsh to start a remote shell
+        qrsh -l short
+
+        # Change back to the original working directory
+        cd "$cwd"
+
+        # Load the required module
+        module load apps/binapps/gaussian/g16c01_em64t
+
+        # Run the gv command with the provided file name in the background
+        gv "$cwd/$1" &
+
+        # Exit the remote shell
+        exit
 }
+
