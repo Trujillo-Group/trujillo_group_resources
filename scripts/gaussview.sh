8752be9272a692dbc878023614681137935f0877
James <144615809+JamesOBrien2@users.noreply.github.com>
2023-09-14 16:14:19 +0100

    Updated gaussview.sh
    Fixed commands following qrsh not running until remote shell was exited.

--- scripts/gaussview.sh
+++ scripts/gaussview.sh
@@ -1,20 +1,15 @@
-function gaussview {
+# Gaussview function
+gaussview() {
         # Store the current working directory
         cwd=$(pwd)
 
         # Run qrsh to start a remote shell
-        qrsh -l short
-
-        # Change back to the original working directory
-        cd "$cwd"
+        qrsh -l short <<EOF
 
         # Load the required module
         module load apps/binapps/gaussian/g16c01_em64t
-
-        # Run the gv command with the provided file name in the background
-        gv "$cwd/$1" &
-
-        # Exit the remote shell
-        exit
+      
+        # Run the gv command with the provided file name
+        gv "$cwd/$1"
+EOF
 }
-
