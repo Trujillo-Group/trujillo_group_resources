e2816016a6f3e3f550f022747b4af6d17a7406e4
James <144615809+JamesOBrien2@users.noreply.github.com>
2023-10-17 17:56:51 +0100

    Update jobs_csf3.sh
    Added total_jobs

--- scripts/submission/CSF3-4/QoL/jobs_csf3.sh
+++ scripts/submission/CSF3-4/QoL/jobs_csf3.sh
@@ -6,9 +6,11 @@ running_jobs=$(qstat -u $USER | awk '$5 == "r" {print}' | wc -l)
 # Get the number of pending jobs
 pending_jobs=$(qstat -u $USER | awk '$5 == "qw" {print}' | wc -l)
 
+# Get the total number of jobs
+total_jobs=$((running_jobs + pending_jobs))
+
 # Print the results
 echo "You currently have:"
 echo "  $running_jobs jobs running"
 echo "  $pending_jobs jobs pending"
-
-
+echo "Total: $total_jobs" 
