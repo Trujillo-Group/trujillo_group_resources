8a902b5c047a3366fd52dee9d83d3f9477b64801
James <144615809+JamesOBrien2@users.noreply.github.com>
2023-10-17 17:56:13 +0100

    Update jobs_csf4.sh
    Added total_jobs

--- scripts/submission/CSF3-4/QoL/jobs_csf4.sh
+++ scripts/submission/CSF3-4/QoL/jobs_csf4.sh
@@ -6,9 +6,12 @@ running_jobs=$(squeue -u $USER | awk '$7 == "R" {print}' | wc -l)
 # Get the number of pending jobs
 pending_jobs=$(squeue -u $USER | awk '$7 == "PD" {print}' | wc -l)
 
+# Get the total number of jobs
+total_jobs=$((running_jobs + pending_jobs))
+
 # Print the results
 echo "You currently have:"
 echo "  $running_jobs jobs running"
 echo "  $pending_jobs jobs pending"
-
+echo "Total: $total_jobs" 
 
