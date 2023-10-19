4949d1af9016525b0d85d50b4fd2d449bcd4c7b6
James <144615809+JamesOBrien2@users.noreply.github.com>
2023-10-19 15:07:35 +0100

    Added check for completed jobs

--- scripts/submission/CSF3-4/QoL/jobs_csf4.sh
+++ scripts/submission/CSF3-4/QoL/jobs_csf4.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# N.B. For this script to work you must have a bin folder in your home directory. (This script will create the necessary files in this folder.)
+
+# This script will check the status of your jobs on CSF4
+# and print the number of running and pending jobs.
+
+# Define the colors
+GREEN=$(tput setaf 2)
+DEFAULT=$(tput sgr0)
+
+echo "${GREEN}--------------------------${DEFAULT}"
+echo "${GREEN}------ Current Jobs ------${DEFAULT}"
+echo "${GREEN}--------------------------${DEFAULT}"
 
 # Get the number of running jobs
 running_jobs=$(squeue -u $USER | awk '$7 == "R" {print}' | wc -l)
@@ -13,5 +25,42 @@ total_jobs=$((running_jobs + pending_jobs))
 echo "You currently have:"
 echo "  $running_jobs jobs running"
 echo "  $pending_jobs jobs pending"
-echo "Total: $total_jobs" 
+echo "Total: $total_jobs"
 
+# Print the job IDs and names
+
+echo "${GREEN}--------------------------${DEFAULT}"
+echo "${GREEN}----- Completed Jobs -----${DEFAULT}"
+echo "${GREEN}--------------------------${DEFAULT}"
+
+# Run the squeue command and store the output in a temporary file
+squeue -u $USER > /mnt/iusers01/chem01/$USER/bin/squeue_jobs.txt
+
+# Use awk to remove the first column and then use the column command to format the output into two separate columns
+awk '{print $1, $4}' /mnt/iusers01/chem01/$USER/bin/squeue_jobs.txt | column -t > /mnt/iusers01/chem01/$USER/bin/current_jobs.txt
+
+# Remove the temporary file
+rm /mnt/iusers01/chem01/$USER/bin/squeue_jobs.txt
+
+# Get the squeue information for user $USER
+squeue -u $USER > /mnt/iusers01/chem01/$USER/bin/squeue_jobs.txt
+
+# Compare the job IDs and save the unmatched job IDs to completed_jobs.txt
+comm -3 <(awk '{print $1}' /mnt/iusers01/chem01/$USER/bin/current_jobs.txt | sort) <(awk '{print $1}' /mnt/iusers01/chem01/$USER/bin/squeue_jobs.txt | sort) > /mnt/iusers01/chem01/$USER/bin/completed_jobs.txt
+
+# Check if completed_jobs.txt is empty
+if [ -s /mnt/iusers01/chem01/$USER/bin/completed_jobs.txt ]; then
+  # Print the number of completed jobs
+  completed_jobs_count=$(wc -l < /mnt/iusers01/chem01/$USER/bin/completed_jobs.txt)
+  echo "Number of completed jobs: $completed_jobs_count"
+
+  # Add a timestamp to the completed jobs and append them to completed_jobs.txt
+  date +"%Y-%m-%d %T" | awk -v OFS='\t' '{print $0}' >> /mnt/iusers01/chem01/$USER/bin/completed_jobs.txt
+else
+  echo "All jobs are still running."
+fi
+
+#echo "----------------------"
+
+# Delete the squeue_jobs.txt file
+rm -f /mnt/iusers01/chem01/$USER/bin/squeue_jobs.txt
