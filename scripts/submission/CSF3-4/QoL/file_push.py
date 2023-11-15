e4b256ca479a358c7c1d3ad01daed583f3fe10f5
James <144615809+JamesOBrien2@users.noreply.github.com>
2023-11-15 13:56:45 +0000

    Added rsync file push/pull scripts for CSF3

---
+++ scripts/submission/CSF3-4/QoL/file_push.py
@@ -0,0 +1,17 @@
+#!/usr/bin/Python3
+import sys
+import subprocess
+
+# Get the source directory from the command line argument
+file = sys.argv[1]
+source_dir = sys.argv[2]
+
+# Locating the username
+result = subprocess.run(['whoami'], capture_output=True, text=True)
+username = result.stdout.strip()
+
+# Construct the rsync command with the source and destination directories
+rsync_cmd = ['rsync', '-avz', f'{file}', f'{username}@csf3.itservices.manchester.ac.uk:{source_dir}']
+
+# Execute the rsync command
+subprocess.run(rsync_cmd)
