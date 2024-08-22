e852de04f98eddf8eaa72b8ac9ba16bddaeb323a
James <144615809+JamesOBrien2@users.noreply.github.com>
2024-08-22 18:34:51 +0100

    Changed myfunctions.py to inigo_functions.py

--- scripts/analysis/gaussian_to_csv.py
+++ scripts/analysis/gaussian_to_csv.py
@@ -14,7 +14,7 @@ import glob
 import csv
 
 # Personal module, in the same folder
-import myfunctions as mf
+import inigo_functions as inf
 
 # Select the desired files by argument and look for them
 try:
@@ -50,49 +50,49 @@ for file in log_files:
     name = file.split(".")[0]
     
     try:
-        sym = text[mf.last_find(text, 'point group')].split()[3]
+        sym = text[inf.last_find(text, 'point group')].split()[3]
     except (IndexError, UnboundLocalError):
         sym = "?"
         
     try:
-        frq = mf.all_find(text, 'Frequencies')
+        frq = inf.all_find(text, 'Frequencies')
         frq[0][0]
         frq = str(frq).replace('--','').count('-')
     except IndexError:
         frq = "?"
 
     try:    
-        scf = text[mf.last_find(text, 'SCF Done')].split()[4]
+        scf = text[inf.last_find(text, 'SCF Done')].split()[4]
     except (IndexError, UnboundLocalError):
         scf = "?"
     
     try:
-        zpe = text[mf.last_find(text, 'Zero-point cor')].split()[2]
+        zpe = text[inf.last_find(text, 'Zero-point cor')].split()[2]
     except (IndexError, UnboundLocalError):
         zpe = "?"
         
     try:
-        nrg = text[mf.last_find(text, 'Sum of electronic and zero')].split()[6]
+        nrg = text[inf.last_find(text, 'Sum of electronic and zero')].split()[6]
     except (IndexError, UnboundLocalError):
         nrg = "?"
   
     try: 
-        dpl = text[mf.last_find(text, 'Tot=')].split()[7]
+        dpl = text[inf.last_find(text, 'Tot=')].split()[7]
     except (IndexError, UnboundLocalError):
         dpl = "?"
         
     try:
-        th = text[mf.last_find(text, 'Thermal correction to Gibbs Free Energy')].split()[6]
+        th = text[inf.last_find(text, 'Thermal correction to Gibbs Free Energy')].split()[6]
     except (IndexError, UnboundLocalError):
         th = "?"
         
     try:
-        dG = text[mf.last_find(text, 'Sum of electronic and thermal Free Energies')].split()[7]
+        dG = text[inf.last_find(text, 'Sum of electronic and thermal Free Energies')].split()[7]
     except (IndexError, UnboundLocalError):
         dG = "?"
         
     try:
-        mp2_raw =text[mf.last_find(text, 'EUMP2')].split()[5].split('D')
+        mp2_raw =text[inf.last_find(text, 'EUMP2')].split()[5].split('D')
         mp2 = "{:.8f}".format(float(mp2_raw[0])*10**int(mp2_raw[1]))
     except (IndexError, UnboundLocalError):
         mp2 = "?"
@@ -101,11 +101,11 @@ for file in log_files:
     end_text = text[-5:]
     
     try:
-        end_text[mf.last_find(end_text, ' Normal termination')]
+        end_text[inf.last_find(end_text, ' Normal termination')]
         state = 'OK'
     except:
         try:
-            end_text[mf.last_find(end_text, ' Error')]
+            end_text[inf.last_find(end_text, ' Error')]
             state = 'ERR'
         except:
             state = 'RUN'
